82e007a773e906e11c58d78c7fcfaad0af79c8c1
Jonathan González <jgonf@safebytelabs.com>
2024-03-25 22:33:11 +0100

    project initialization

---
+++ src/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# pylint: disable=W0102,E0712,C0103
+
+""" GITHUB ACTIONS - MOVE PROJECT CARDS """
+
+# TODO: Put your messages here
+
+# general imports
+# third-party imports
+# local imports
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
