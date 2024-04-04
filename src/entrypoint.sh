8852c5c83eb4db54391a9682f902816904861719
Jonathan González <jgonf@safebytelabs.com>
2024-04-04 23:56:20 +0200

    new code base

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,1 +1,17 @@
 #!/bin/ash
+
+# Delay initialization, in case this is desired for whatever reason.
+
+delaystart=$1
+if [ -z "${delaystart}" ]
+then
+    delaystart=0
+fi
+sleep $delaystart
+
+#
+# Write the rest of the script here
+#
+
+exit 0
+##
